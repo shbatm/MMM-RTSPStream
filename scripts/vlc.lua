615d8214fe213db10e059a61a55687b63dd5809d
shbatm <support@shbatm.com>
2019-01-03 21:57:15 -0600

    Fix permissions requirements on install

--- scripts/vlc.lua
+++ scripts/vlc.lua
@@ -1,9 +1,7 @@
 
-local function starts_with(str, start)
-   return str:sub(1, #start) == start
-end
-if (starts_with(get_window_name(), "stream")) then
-    set_window_geometry(642, 55, 540, 405);
+if (get_window_name()=="stream3") then
+    set_window_geometry(495, 619, 630, 430);
     undecorate_window();
     set_on_top();
+    make_always_on_top();
 end
