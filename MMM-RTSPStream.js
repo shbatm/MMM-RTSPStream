ed5d707b62d321aef81cf39816853907e85b4129
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2023-11-21 18:55:00 +0100

    MagicMirror²

--- MMM-RTSPStream.js
+++ MMM-RTSPStream.js
@@ -1,6 +1,6 @@
 /* global Module */
 
-/* Magic Mirror
+/* MagicMirror²
  * Module: MMM-RTSPStream
  *
  * By shbatm
--- node_helper.js
+++ node_helper.js
@@ -1,4 +1,4 @@
-/* Magic Mirror
+/* MagicMirror²
  * Node Helper: MMM-RTSPStream
  *
  * By shbatm
